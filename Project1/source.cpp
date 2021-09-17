17ac857dda3c987d41183cca9fa7cfd0efca88c7
Aiden <aiden.na81@gmail.com>
2021-09-17 17:52:09 +0900

    별그리기      *     **    ***   ****  *****

--- Project1/source.cpp
+++ Project1/source.cpp
@@ -4,16 +4,24 @@ using namespace std;
 
 int main()
 {
-	int MyGold = 0;
-	float HP = 0.12332f;
-	char h = 'H';
-	char e = 'E';
-	char l = 'L';
-	char o = 'O';
+	int max = 0;
+	cin >> max;
 
-	cout << MyGold << endl;
-	cout << h << e << l << l << o << endl;
-	cout << "sdfsd" << endl;
+	for (int i = 0; i < max; i++)
+	{
 
+		for (int j = max; j > i; j--)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "*";
+		}
+
+		cout << endl;
+
+
+	}
 	return 0;
 }
