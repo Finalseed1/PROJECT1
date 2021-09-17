dfdfa6182e45a406dd8bd129429d5e7f5e200c4e
Aiden <aiden.na81@gmail.com>
2021-09-17 09:30:45 +0900

    int float char

---
+++ Project1/소스.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int MyGold = 0;
+	float HP = 0.111242f;
+	char h = 'H';
+	char e = 'E';
+	char l = 'L';
+	char o = 'O';
+
+	cout << MyGold << endl;
+	cout << HP << endl;
+	cout << h << e << l << l << o << endl;
+
+	return 0;
+}
